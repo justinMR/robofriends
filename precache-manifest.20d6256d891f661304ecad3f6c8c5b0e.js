989aa4470bac27eb24ab6bcf66444e52b4c108c7
Justin Michael Ramirez <JustinMichaelRamirez@gmail.com>
2019-09-21 13:15:58 -0400

    Updates

---
+++ precache-manifest.20d6256d891f661304ecad3f6c8c5b0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963904f900e39511684497f84c9e08d8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2b4ae4f679bdf8a07ba8",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "2ae4b942d215c60432a7",
+    "url": "/robofriends/static/css/main.72070335.chunk.css"
+  },
+  {
+    "revision": "2b4ae4f679bdf8a07ba8",
+    "url": "/robofriends/static/js/2.f7eb401d.chunk.js"
+  },
+  {
+    "revision": "2ae4b942d215c60432a7",
+    "url": "/robofriends/static/js/main.2a54363d.chunk.js"
+  },
+  {
+    "revision": "d7d3c3ad781ab5e09c65",
+    "url": "/robofriends/static/js/runtime-main.a9498424.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
